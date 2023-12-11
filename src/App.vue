89412f89075b73d0eddd815ae45e27473c5ed1ad
fbaltaci <fbaltaci@advisor360.com>
2023-12-10 19:33:50 -0500

    init commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <!-- I have my navigation menu here -->
+  <header class="header container">
+    <HeaderIcon></HeaderIcon>
+    <HeaderNav></HeaderNav>
+  </header>
+
+  <router-view />
+
+  <!-- I have my footer menu here -->
+  <footer class="footer container">
+    <FooterNav></FooterNav>
+  </footer>
+</template>
+
+<script>
+import HeaderIcon from "@/components/HeaderIcon.vue";
+import HeaderNav from "@/components/HeaderNav.vue";
+import FooterNav from "@/components/FooterNav.vue";
+
+export default {
+  components: {
+    HeaderIcon,
+    HeaderNav,
+    FooterNav,
+  },
+};
+</script>
+
+<style></style>
